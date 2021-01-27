80a2218f0d329eb51d198513fd7024570e978819
PAYEL <pp2659@srmist.edu.in>
2021-01-27 18:09:52 +0530

    A mi card or personal information.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,105 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/payel.jpeg'),
+
+              ),
+              Text(
+                'Payel Paul',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize:40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height:5.0,
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                 style: TextStyle(
+                fontFamily: 'Bebas',
+                fontSize:20.0,
+                letterSpacing: 2.5,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 200.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                color:Colors.white,
+
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                 padding: EdgeInsets.all(10.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.call,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+91 6290922***',
+                      style: TextStyle(
+
+                        color: Colors.teal,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color:Colors.white,
+
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.mail,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'payel.6589@gmail.com',
+                      style: TextStyle(
+
+                        color: Colors.teal,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
